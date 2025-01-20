9d96f60ea94b01c466c11dbac443d0aa740f0d80
Ola <a.czarniecka03@gmail.com>
2025-01-20 09:41:30 +0100

    parser update

--- compiler/parser.py
+++ compiler/parser.py
@@ -10,221 +10,265 @@ class my_parser(Parser):
     literal_constants = set()
 
     precedence = (
-        ('left', 'PLUS', 'MINUS'),
-        ('left', 'MULTIPLY', 'DIVIDE', 'MOD')
+        ('left', 'PLUS', 'MINUS'), 
+        ('left', 'MULTIPLY', 'DIVIDE', 'MOD') 
     )
 
-    # program_all
-    @_('procedures main')
+    # program_all: program składa się z procedur (opcjonalne) i głównego programu main
+    @_('procedures main') # type: ignore
     def program_all(self, p):
-        pass
-    
-    # procedures
-    @_('procedures PROCEDURE proc_head IS declarations BEGIN commands END')
-    def procedures(self, p):
-        pass
-    @_('procedures PROCEDURE proc_head IS BEGIN commands END')
-    def procedures(self, p):
-        pass
-    @_('')
-    def procedures(self, p):
-        return []
-    
-    # main
-    @_('PROGRAM IS declarations BEGIN commands END')
-    def main(self, p):
-        pass
-    @_('PROGRAM IS BEGIN commands END')
-    def main(self, p):
+        #self.code = Generator(p.commands, self.symbol_table)
+        #return self.code
         pass
 
-    # commands
-    @_('commands command')
+
+    # procedures: funkcje
+    @_('procedures PROCEDURE proc_head IS declarations BEGIN commands END') # type: ignore
+    def procedures(self, p):
+        #procedure = "PROCEDURE", p[2], p[4], p[6], self.literal_constants.copy()
+        #self.literal_constants.clear()
+        #return procedure
+        pass
+    @_('procedures PROCEDURE proc_head IS BEGIN commands END') # type: ignore
+    def procedures(self, p):
+        pass
+    @_('') # type: ignore
+    def procedures(self, p):
+        return []
+        #return pass
+
+
+    # main: główna część programu
+    @_('PROGRAM IS declarations BEGIN commands END') # type: ignore
+    def main(self, p):
+        return "MAIN_DEC", p[2], p[4]
+    @_('PROGRAM IS BEGIN commands END') # type: ignore
+    def main(self, p):
+        return "MAIN", p[3]
+
+
+    # commands: komendy
+    @_('commands command') # type: ignore
     def commands(self, p):
         return p[0] + [p[1]]
-    @_('command')
+    @_('command') # type: ignore
     def commands(self, p):
         return [p[0]]
 
+
     # command
-    @_('identifier ASSIGN expression SEMICOLON')
+    @_('identifier ASSIGN expression SEMICOLON') # type: ignore
     def command(self, p):
         return "ASSIGN", p[0], p[2]
 
-    @_('IF condition THEN commands ELSE commands ENDIF')
+    @_('IF condition THEN commands ELSE commands ENDIF') # type: ignore
     def command(self, p):
         if_else_com = "IFELSE", p[1], p[3], p[5], self.literal_constants.copy()
         self.literal_constants.clear()
         return if_else_com
     
-    @_('IF condition THEN commands ENDIF')
+    @_('IF condition THEN commands ENDIF') # type: ignore
     def command(self, p):
         if_com = "IF", p[1], p[3], self.literal_constants.copy()
         self.literal_constants.clear()
         return if_com
     
-    @_('WHILE condition DO commands ENDWHILE')
+    @_('WHILE condition DO commands ENDWHILE') # type: ignore
     def command(self, p):
         while_com = "WHILE", p[1], p[3], self.literal_constants.copy()
         self.literal_constants.clear()
         return while_com
     
-    @_('REPEAT commands UNTIL condition SEMICOLON')
+    @_('REPEAT commands UNTIL condition SEMICOLON') # type: ignore
     def command(self, p):
         repeat_com = "REPEAT", p[3], p[1], self.literal_constants.copy()
         self.literal_constants.clear()
         return repeat_com
     
-    @_('FOR PIDENTIFIER FROM value TO value DO commands ENDFOR')  
+    @_('FOR PIDENTIFIER FROM value TO value DO commands ENDFOR')  # type: ignore
     def command(self, p):
         for_to_com = "FORTO", p[1], p[3], p[5], p[7], self.literal_constants.copy()
         self.literal_constants.clear()
         return for_to_com
      
-    @_('FOR PIDENTIFIER FROM value DOWNTO value DO commands ENDFOR')   
+    @_('FOR PIDENTIFIER FROM value DOWNTO value DO commands ENDFOR')   # type: ignore
     def command(self, p):
         for_downto_com = "FORDOWNTO", p[1], p[3], p[5], p[7], self.literal_constants.copy()
         self.literal_constants.clear()
         return for_downto_com
      
-    @_('proc_call SEMICOLON')
+    # wywołanie procedury
+    @_('proc_call SEMICOLON') # type: ignore
     def command(self, p):
         pass
 
-    @_('READ identifier SEMICOLON')
+    @_('READ identifier SEMICOLON') # type: ignore
     def command(self, p):
         return "READ", p[1]
     
-    @_('WRITE value SEMICOLON')
+    @_('WRITE value SEMICOLON') # type: ignore
     def command(self, p):
         if p[1][0] == "NUM":
             self.literal_constants.add(int(p[1][1]))
         return "WRITE", p[1]
 
-    # proc_head
-    @_('PIDENTIFIER LPAREN args_decl RPAREN')
+   
+    # proc_head: nagłówek procedury
+    @_('PIDENTIFIER LPAREN args_decl RPAREN') # type: ignore
     def proc_head(self, p):
         pass
 
-    # proc_call
-    @_('PIDENTIFIER LPAREN args RPAREN')
+   
+    # proc_call: wywołanie procedury
+    @_('PIDENTIFIER LPAREN args RPAREN') # type: ignore
     def proc_call(self, p):
         pass
 
+    
     # declarations 
-    @_('declarations COMMA PIDENTIFIER')
+    @_('declarations COMMA PIDENTIFIER') # type: ignore
     def declarations(self, p):
-        pass
-    @_('declarations COMMA PIDENTIFIER LBRACKET NUM COLON NUM RBRACKET')
-    def declarations(self, p):
-        pass
-    @_('PIDENTIFIER')
-    def declarations(self, p):
-        pass
-    @_('PIDENTIFIER LBRACKET NUM COLON NUM RBRACKET')
-    def declarations(self, p):
-        pass
+        self.symbol_table.add_variable(p[2]) #????????????????????????????
 
+    @_('declarations COMMA PIDENTIFIER LBRACKET NUM COLON NUM RBRACKET') # type: ignore
+    def declarations(self, p):
+        self.symbol_table.add_array(p[2], p[4], p[6])
+
+    @_('PIDENTIFIER') # type: ignore
+    def declarations(self, p):
+        self.symbol_table.add_variable(p[0])
+
+    @_('PIDENTIFIER LBRACKET NUM COLON NUM RBRACKET') # type: ignore
+    def declarations(self, p):
+        self.symbol_table.add_array(p[0], p[2], p[4])
+
+    
     # args_decl
-    @_('args_decl COMMA PIDENTIFIER')
+    @_('args_decl COMMA PIDENTIFIER') # type: ignore
     def args_decl(self, p):
         pass
-    @_('args_decl COMMA T PIDENTIFIER')
+    @_('args_decl COMMA T PIDENTIFIER') # type: ignore
     def args_decl(self, p):
         pass
-    @_('PIDENTIFIER')
+    @_('PIDENTIFIER') # type: ignore
     def args_decl(self, p):
         pass
-    @_('T PIDENTIFIER')
+    @_('T PIDENTIFIER') # type: ignore
     def args_decl(self, p):
         pass
 
+    
     # args
-    @_('args COMMA PIDENTIFIER')
+    @_('args COMMA PIDENTIFIER') # type: ignore
     def args(self, p):
         pass
-    @_('PIDENTIFIER')
+    @_('PIDENTIFIER') # type: ignore
     def args(self, p):
         pass
 
+   
     # expression
-    @_('value')
+    @_('value') # type: ignore
     def expression(self, p):
         return p[0]
     
-    @_('value PLUS value')
+    @_('value PLUS value') # type: ignore
     def expression(self, p):
         return "PLUS", p[0], p[2]
     
-    @_('value MINUS value')
+    @_('value MINUS value') # type: ignore
     def expression(self, p):
         return "MINUS", p[0], p[2]
     
-    @_('value MULTIPLY value')
+    @_('value MULTIPLY value') # type: ignore
     def expression(self, p):
         return "MULTIPLY", p[0], p[2]
     
-    @_('value DIVIDE value')
+    @_('value DIVIDE value') # type: ignore
     def expression(self, p):
         return "DIVIDE", p[0], p[2]
     
-    @_('value MOD value')
+    @_('value MOD value') # type: ignore
     def expression(self, p):
         return "MOD", p[0], p[2]
 
+   
     # condition 
-    @_('value EQUAL value')
+    @_('value EQUAL value') # type: ignore
     def condition(self, p):
         return "EQUAL", p[0], p[2]
     
-    @_('value NEQUAL value')
+    @_('value NEQUAL value') # type: ignore
     def condition(self, p):
         return "NEQUAL", p[0], p[2]
     
-    @_('value GREATER value')
+    @_('value GREATER value') # type: ignore
     def condition(self, p):
         return "GREATER", p[0], p[2]
     
-    @_('value LESS value')
+    @_('value LESS value') # type: ignore
     def condition(self, p):
         return "LESS", p[0], p[2]
     
-    @_('value GEQ value')
+    @_('value GEQ value') # type: ignore
     def condition(self, p):
         return "GEQ", p[0], p[2]
     
-    @_('value LEQ value')
+    @_('value LEQ value') # type: ignore
     def condition(self, p):
         return "LEQ", p[0], p[2]
 
+    
     # value
-    @_('NUM')
+    @_('NUM') # type: ignore
     def value(self, p):
         return "NUM", p[0]
     
-    @_('identifier')
+    @_('identifier') # type: ignore
     def value(self, p):
         return "ID", p[0]
 
+    
     # identifier
-    @_('PIDENTIFIER')
+    @_('PIDENTIFIER') # type: ignore
     def identifier(self, p):
         if p[0] in self.symbol_table or p[0] in self.symbol_table.iterators:
             return p[0]
         else:
             return "UNDECLARED", p[0]
         
-    @_('PIDENTIFIER LBRACKET PIDENTIFIER RBRACKET')
+    @_('PIDENTIFIER LBRACKET PIDENTIFIER RBRACKET') # type: ignore
     def identifier(self, p):
-        pass
+        if p[0] in self.symbol_table and type(self.symbol_table[p[0]]) == Array:
+            if p[2] in self.symbol_table and type(self.symbol_table[p[2]]) == Variable:
+                return "ARRAY", p[0], ("ID", p[2])
+            else:
+                return "ARRAY", p[0], ("ID", ("UNDECLARED", p[2]))
+        else:
+            raise ValueError(f"ERROR: undeclared array {p[0]}")
         
-    @_('PIDENTIFIER LBRACKET NUM RBRACKET')
+    @_('PIDENTIFIER LBRACKET NUM RBRACKET') # type: ignore
     def identifier(self, p):
-        pass
+        if p[0] in self.symbol_table and type(self.symbol_table[p[0]]) == Array:
+            return "ARRAY", p[0], p[2]
+        else:
+            raise Exception(f"ERROR: undeclared array {p[0]}")
 
     # -----------------------------------------------
     def error(self, p):
         if p:
-            print(f'Syntax error: {p.type}')
+            print(f'Syntax error: {p.type} with {p.value}')
         else:
             print("Syntax error at EOF")
+
+lex = my_lexer()
+pars = my_parser()
+text = ' x := 2 '
+data1 = '''PROGRAM IS
+BEGIN
+    x := 2;
+END'''
+a = lex.tokenize(data1)
+for token in a:
+        print(token)
+pars.parse(a)
